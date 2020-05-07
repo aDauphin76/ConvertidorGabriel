6816ee0825c1ee610fbfcb9fb2489f66a78e2217
Gabriel Dauphin <adauphin76@gmail.com>
2020-05-07 00:42:24 -0300

    Tarea del modulo de peso

--- app/src/main/java/com/example/convertidor/Peso.java
+++ app/src/main/java/com/example/convertidor/Peso.java
@@ -15,6 +15,8 @@ public class Peso extends AppCompatActivity {
 
     EditText etEntradaPeso;
     Button bConvertir;
+    RadioButton rbKGDE, rbGDE, rbMGDE, rbTMDE, rbTIDE, rbLIBRADE,rbOZDE, rbKGA, rbGA, rbMGA, rbTMA,
+            rbTIA, rbLIBRAA, rbOZA;
     Float intermedioDe;
     Float resultadoA;
 
@@ -41,44 +43,89 @@ public class Peso extends AppCompatActivity {
         boolean checked = ((RadioButton) view).isChecked();
         Float entradaPesoFLT    = Float.parseFloat(entradaPesoSTR);
 
+        rbKGA       = findViewById(R.id.rbKGA);
+        rbGA        = findViewById(R.id.rbGA);
+        rbMGA       = findViewById(R.id.rbMGA);
+        rbTMA       = findViewById(R.id.rbTMA);
+        rbTIA       = findViewById(R.id.rbTIA);
+        rbLIBRAA    = findViewById(R.id.rbLIBRAA);
+        rbOZA       = findViewById(R.id.rbOZA);
+
+        habilitarTodos(1);
+
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.rbKGDE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT;
-                    break;
+                    rbKGA.setEnabled(false);
+                }
+                break;
 
             case R.id.rbGDE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT / 1000;
+                    rbGA.setEnabled(false);
+                }
                 break;
 
             case R.id.rbMGDE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT / 1000000;
+                    rbMGA.setEnabled(false);
+                }
                 break;
 
             case R.id.rbTMDE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT * 1000;
+                    rbTMA.setEnabled(false);
+                }
                 break;
 
             case R.id.rbTIDE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT * 907.03f;
+                    rbTIA.setEnabled(false);
+                }
                 break;
 
             case R.id.rbLIBRADE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT / 2.205f;
+                    rbLIBRAA.setEnabled(false);
+                }
                 break;
 
             case R.id.rbOZDE:
-                if (checked)
+                if (checked){
                     intermedioDe = entradaPesoFLT * 0.02835f;
+                    rbOZA.setEnabled(false);
+                }
                 break;
         }
     }
+
+    private void habilitarTodos(int i) {
+        if (i == 1) {
+            rbKGA.setEnabled(true);
+            rbGA.setEnabled(true);
+            rbMGA.setEnabled(true);
+            rbTMA.setEnabled(true);
+            rbTIA.setEnabled(true);
+            rbLIBRAA.setEnabled(true);
+            rbOZA.setEnabled(true);
+        }else{
+            rbKGDE.setEnabled(true);
+            rbGDE.setEnabled(true);
+            rbMGDE.setEnabled(true);
+            rbTMDE.setEnabled(true);
+            rbTIDE.setEnabled(true);
+            rbLIBRADE.setEnabled(true);
+            rbOZDE.setEnabled(true);
+        }
+    }
+
 
     public void SelecionadoA(View view) {
 
@@ -91,42 +138,66 @@ public class Peso extends AppCompatActivity {
         bConvertir.setEnabled(true);
         boolean checked = ((RadioButton) view).isChecked();
 
+        rbKGDE      = findViewById(R.id.rbKGDE);
+        rbGDE       = findViewById(R.id.rbGDE);
+        rbMGDE      = findViewById(R.id.rbMGDE);
+        rbTMDE      = findViewById(R.id.rbTMDE);
+        rbTIDE      = findViewById(R.id.rbTIDE);
+        rbLIBRADE   = findViewById(R.id.rbLIBRADE);
+        rbOZDE      = findViewById(R.id.rbOZDE);
+
+        habilitarTodos(0);
+
         // Check which radio button was clicked
         switch(view.getId()) {
 
             case R.id.rbKGA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe;
+                    rbKGDE.setEnabled(false);
+                }
                 break;
 
             case R.id.rbGA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe * 1000;
+                    rbGDE.setEnabled(false);
+                }
                 break;
 
             case R.id.rbMGA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe * 1000000;
+                    rbMGDE.setEnabled(false);
+                }
                 break;
 
             case R.id.rbTMA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe / 1000;
+                    rbTMDE.setEnabled(false);
+                }
                 break;
 
             case R.id.rbTIA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe / 907.03f;
+                    rbTIDE.setEnabled(false);
+                }
                 break;
 
             case R.id.rbLIBRAA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe * 2.205f;
+                    rbLIBRADE.setEnabled(false);
+                }
                 break;
 
             case R.id.rbOZA:
-                if (checked)
+                if (checked){
                     resultadoA = intermedioDe / 0.02835f;
+                    rbOZDE.setEnabled(false);
+                }
                 break;
         }
     }
